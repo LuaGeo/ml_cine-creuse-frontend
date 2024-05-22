ae66b852b878f9139e22ce3163659d32c6fd3636
luageo <ldeoliveiratech@gmail.com>
2024-05-22 21:30:55 +0200

    debugging logs added to fix favorites button

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,32 @@
 import { Link } from "react-router-dom";
 import Stars from "./Stars.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const MovieCard = ({ movie, userId }) => {
   const [isFavorite, setIsFavorite] = useState(movie.isFavorite);
 
-  const handleFavoriteClick = async (movieId) => {
+  useEffect(() => {
+    console.log(`Favorite state for movie ${movie.titleId}: ${isFavorite}`);
+  }, [isFavorite]);
+
+  const handleFavoriteClick = async (event, movieId) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    console.log("handleFavoriteClick was called");
+
     try {
       if (isFavorite) {
-        await axios.delete(`http://127.0.0.1:5000/favorites/`, {
+        await axios.delete(`http://127.0.0.1:5000/favorites`, {
           data: {
-            movieId: titleId,
+            movieId,
             userId,
           },
         });
       } else {
-        await axios.post("http://127.0.0.1:5000/favorites/", {
-          movieId: titleId,
+        await axios.post("http://127.0.0.1:5000/favorites", {
+          movieId,
           userId,
         });
       }
@@ -27,26 +36,28 @@ const MovieCard = ({ movie, userId }) => {
     }
   };
   return (
-    <div key={movie.titleId} className="carousel-item">
-      <Link to={`/movie/${movie.titleId}`}>
+    <Link to={`/movie/${movie.titleId}`}>
+      <div key={movie.titleId} className="carousel-item">
         <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
         />
-      </Link>
-      <div className="details-movie-card-container">
-        <div>
-          <h3>{movie.title}</h3>
-          <Stars movie={movie} />
-        </div>
-        <div className="bottom-movie-card">
-          <button onClick={() => handleFavoriteClick(movie.titleId)}>
-            {isFavorite ? "✓" : "+"}
-          </button>
-          <p>{movie.main_genre}</p>
+        <div className="details-movie-card-container">
+          <div>
+            <h3>{movie.title}</h3>
+            <Stars movie={movie} />
+          </div>
+          <div className="bottom-movie-card">
+            <button
+              onClick={(event) => handleFavoriteClick(event, movie.titleId)}
+            >
+              {isFavorite ? "✓" : "+"}
+            </button>
+            <p>{movie.main_genre}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
